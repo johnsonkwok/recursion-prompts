78bc46649d29122deb689f7c24884991bf19ce8a
Johnson Kwok <johnsonkwok1@gmail.com>
2018-10-10 21:56:42 -0700

    Implement the 12)multiply and 13)divide functions
    * Refactor the modulo function in order to accommodate more cases

--- src/recursion.js
+++ src/recursion.js
@@ -144,11 +144,11 @@ var modulo = function(x, y) {
     return NaN;
   } else if (x === y || x === 0) {
     return 0;
-  } else if ((x > 0 && y > 0 && x < y) || (x < 0 && y < 0 && x > y) || (x < 0 && y > 0 && x > -y)) {
+  } else if ((x > 0 && y > 0 && x < y) || (x > 0 && y < 0 && x < -y) || (x < 0 && y < 0 && x > y) || (x < 0 && y > 0 && x > -y)) {
     return x;
   } else if ((x > 0 && y > 0) || (x < 0 && y < 0)) {
     return modulo((x - y), y);
-  } else if (x < 0 && y > 0) {
+  } else {
     return modulo((x + y), y);
   }
 };
@@ -156,11 +156,31 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if (x === 0 || y === 0) {
+    return 0;
+  } else if (y > 0) {
+    return x + multiply(x, (y - 1));
+  } else {
+    return -x + multiply(x, (y + 1));
+  }
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
 var divide = function(x, y) {
+  if (y === 0) {
+    return NaN;
+  } else if (x === 0) {
+    return 0;
+  } else if (x > 0 && y > 0) {
+    return (x < y) ? 0 : 1 + divide((x - y), y);
+  } else if (x < 0 && y < 0) {
+    return (x > y) ? 0 : 1 + divide((x - y), y);
+  } else if (x < 0 && y > 0) {
+    return (-x < y) ? 0 : -1 + divide((x + y), y);
+  } else if (x > 0 && y < 0) {
+    return (x < -y) ? 0 : -1 + divide((x + y), y); 
+  }
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
